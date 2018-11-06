6f55e5a52f348b0beead2ed91b89a6644cf268af
Your Name <salil_bhise@yahoo.com>
2018-11-06 02:38:18 -0600

    home and survey html created

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-// Dependencies
-// =============================================================
+// dependencies
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 
-// Sets up the Express App
-// =============================================================
+// sets up the express app
 var app = express();
+
+// connects to heroku
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+// sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// api routes
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// listener
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
